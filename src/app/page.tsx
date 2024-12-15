a1a534bb153dbe974e00e4d864de9db87bef390c
risha.asdiyana <asdiyanarisha@gmail.com>
2024-12-15 08:35:55 +0700

    fix layouting headline

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,7 +23,7 @@ export default function Home() {
               </div>
           </div>
 
-          <div className="mt-52 pt-36">
+          <div className="mt-52 pt-36 shadow-2xl">
               <div className="flex items-center justify-center font-sans bg-black text-white">
                   <div className="w-1/2 my-20">
                       <div className="flex flex-row justify-center">
@@ -32,18 +32,20 @@ export default function Home() {
                               <AvatarFallback>RAR</AvatarFallback>
                           </Avatar>
                       </div>
-                      <div className="my-5">
-                          <p className="text-xl text-wrap">As a backend engineer, I specialize in developing secure,
-                              scalable,
-                              and
-                              high-performance server-side systems. From API development to database optimization, I
-                              ensure
-                              your applications run flawlessly while supporting your business goals. My expertise
-                              lies
-                              in
-                              languages like Golang, Python, and Node.js, with a focus on delivering clean,
-                              maintainable
-                              code.</p>
+                      <div className="flex flex-row justify-center my-5">
+                          <div className="w-full md:w-3/4 md:text-justify text-center">
+                              <p className="text-xl text-wrap">As a backend engineer, I specialize in developing secure,
+                                  scalable,
+                                  and
+                                  high-performance server-side systems. From API development to database optimization, I
+                                  ensure
+                                  your applications run flawlessly while supporting your business goals. My expertise
+                                  lies
+                                  in
+                                  languages like Golang, Python, and Node.js, with a focus on delivering clean,
+                                  maintainable
+                                  code.</p>
+                          </div>
                       </div>
                       <div className="flex pt-6 pb-3 justify-center" id="icon">
                           <div className="flex flex-row gap-4 w-1/2 justify-center">
