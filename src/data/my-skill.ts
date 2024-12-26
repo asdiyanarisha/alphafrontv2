9756d2fbcde273c3c81ef1adcd50a2c3b98b94ea
risha.asdiyana <asdiyanarisha@gmail.com>
2024-12-26 15:05:44 +0700

    get list myskills by data

---
+++ src/data/my-skill.ts
@@ -0,0 +1,23 @@
+export const MySkills = [
+    {
+        name: "Golang",
+        point: 80,
+    },
+    {
+        name: "Python",
+        point: 85,
+    },
+    {
+        name: "Postgres",
+        point: 75,
+    },
+    {
+        name: "Docker",
+        point: 85,
+    },
+    {
+        name: "Mongodb",
+        point: 80,
+    }
+
+];
