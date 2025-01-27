db1a5aabb235cfe8628be737618783e397425440
risha.asdiyana <asdiyanarisha@gmail.com>
2025-01-27 13:20:02 +0700

    add store cookies

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,4 +1,19 @@
+'use server'
 import axiosInstance from "@/lib/axios";
+import {cookies} from "next/headers";
+
+
+const storeToCookies = async (token: string) => {
+    const expiresAt = new Date(Date.now() + 60 * 60 * 1000)
+    const cookieStore = await cookies()
+    cookieStore.set('session', token, {
+        httpOnly: true,
+        secure: true,
+        expires: expiresAt,
+        sameSite: 'lax',
+        path: '/',
+    })
+}
 
 const LoginSubmitAction = async (email: string, password: string) => {
     return await axiosInstance.post("/login", {
@@ -7,9 +22,9 @@ const LoginSubmitAction = async (email: string, password: string) => {
         headers: {
             'Content-Type': 'application/json'
         }
-    }).then(function (response) {
-        console.log("success")
-        return {code: response.status, status: "success"};
+    }).then(async function (response) {
+        await storeToCookies(response.data.token);
+        return {code: response.status, status: "success", token: response.data.token};
     }).catch(function (err) {
         if (err.response.status == 401) {
             return {code: err.response.status, status: "unauthenticated"};
