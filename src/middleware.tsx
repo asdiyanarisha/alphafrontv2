5416409d4635f2a3dc4f010b03885be1437c36e6
risha.asdiyana <asdiyanarisha@gmail.com>
2025-01-30 14:46:55 +0700

    fix middleware when session is expired

--- src/middleware.tsx
+++ src/middleware.tsx
@@ -1,19 +1,21 @@
 import {NextRequest, NextResponse} from "next/server";
 
 export function middleware(request: NextRequest) {
-    const cookie = request.cookies.get('session')?.value
-    if (cookie == undefined) {
+    const session = request.cookies.get('session')?.value
+    console.log(session);
+
+    if (session == undefined) {
         return NextResponse.redirect(new URL('/login', request.url))
     }
 
-    if (request.nextUrl.pathname.startsWith('/login')) {
-        return NextResponse.redirect(new URL('/admin', request.url))
-    }
+    // if (request.nextUrl.pathname.startsWith('/login')) {
+    //     return NextResponse.redirect(new URL('/admin', request.url))
+    // }
 
     return NextResponse.next()
 }
 
 // See "Matching Paths" below to learn more
 export const config = {
-    matcher: ['/admin/:path*', '/login'],
+    matcher: ['/admin/:path*'],
 }
