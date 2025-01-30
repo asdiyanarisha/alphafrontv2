85191b16d8300d0695eebd9b747344f39ca51a2c
risha.asdiyana <asdiyanarisha@gmail.com>
2025-01-30 22:43:33 +0700

    fix layouting list blog

--- src/app/admin/blogs/page.tsx
+++ src/app/admin/blogs/page.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import {Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle} from "@/components/ui/card";
-import {Button} from "@/components/ui/button";
 import Link from "next/link";
-import {CirclePlus} from "lucide-react";
+import {CirclePlus, Ellipsis} from "lucide-react";
+
+import "./styles.css";
 
 const Blogs: React.FC = async () => {
     return (
@@ -13,21 +14,70 @@ const Blogs: React.FC = async () => {
                         <CardTitle>
                             <h1 className="text-3xl font-bold">Blogs</h1>
                         </CardTitle>
-                        <CardDescription className="text-sm text-slate-600">
+                        <CardDescription className="text-sm text-slate-600 pt-1">
                             List of blogs
                         </CardDescription>
                     </div>
                     <div className="pr-2">
-                        <Link className="flex flex-row px-8 py-2.5 min-w-[120px] text-center text-lg text-slate-900 border border-slate-800 rounded-md hover:bg-slate-800 hover:text-white active:bg-indigo-500 focus:outline-none focus:ring"
+                        <Link className="flex flex-row px-4 py-2.5 min-w-[120px] text-[16px] text-center text-slate-900 border border-slate-800 rounded-md hover:bg-slate-800 hover:text-white active:bg-indigo-500 focus:outline-none focus:ring"
                            href="/download">
                             {/*<span><CirclePlus/></span>*/}
-                            <CirclePlus size="20" className="mt-1 mr-1"/>
+                            <CirclePlus size="16" className="mt-1 mr-1"/>
                             <span>Create a post</span>
                         </Link>
                     </div>
                 </CardHeader>
                 <CardContent>
-                    <p>Card Content</p>
+                    <div className="p-3">
+                        <div className="overflow-x-auto">
+                            <table className="table-auto w-full">
+                                <thead className="text-sm font-semibold uppercase text-gray-400 bg-gray-50">
+                                <tr>
+                                    <th className="pr-5 pl-2 whitespace-nowrap">
+                                        <div className="font-semibold text-left">
+                                            <input type="checkbox" className="checkbox peer text-primary" />
+                                        </div>
+                                    </th>
+                                    <th className="col-title">
+                                        <div className="font-semibold text-left">Title</div>
+                                    </th>
+                                    <th className="col-table">
+                                        <div className="font-semibold text-left">Tags</div>
+                                    </th>
+                                    <th className="col-table">
+                                        <div className="font-semibold text-left">Created At</div>
+                                    </th>
+                                    <th className="col-table">
+                                        <div className="font-semibold text-center">Action</div>
+                                    </th>
+                                </tr>
+                                </thead>
+                                <tbody className="text-sm divide-y divide-gray-100">
+                                <tr>
+                                    <td className="pr-5 pl-2">
+                                        <div className="font-semibold text-left">
+                                            <input type="checkbox" className="checkbox peer text-primary"/>
+                                        </div>
+                                    </td>
+                                    <td className="col-title">
+                                        <div className="text-left font-bold">Fundamental of beatifulsoup for python</div>
+                                    </td>
+                                    <td className="col-table">
+                                    <div className="text-left">Python,Scrapper,Crawler</div>
+                                    </td>
+                                    <td className="col-table">
+                                        <div className="text-left font-medium text-green-500">20 Aug 2024</div>
+                                    </td>
+                                    <td className="col-table">
+                                        <div className="text-lg flex justify-center">
+                                            <Ellipsis/>
+                                        </div>
+                                    </td>
+                                </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
                 </CardContent>
                 <CardFooter>
                     <p>Card Footer</p>
