442d0793f90c9b707d1be82f60fbcb6aff9192c4
risha.asdiyana <asdiyanarisha@gmail.com>
2025-02-24 13:59:00 +0700

    fix blog image

---
+++ src/app/(main)/blogs/card.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import {Blog} from "@/app/(main)/blogs/page";
+
+
+const CardBlog = ({ d }: {d: Blog}) => {
+
+    return (
+        <div className="rounded-3xl w-full sm:w-1/2 md:w-1/3 self-stretch mb-2 hover:shadow-2xl border">
+            <div className="h-full">
+                <Link href={'/blogs/' + d.slug} className="">
+                    <div className="relative w-full">
+                        <Image className="m-0 lazy rounded-2xl h-52 w-96" src={d.url_image}
+                               width={400}
+                               height={30}
+                               quality={75} alt=""
+                               style={{
+                                   objectFit: 'cover',
+                               }}
+                        />
+                    </div>
+
+                </Link>
+                <div className="px-6 py-5">
+                    <div className="flex flex-col mb-2">
+                        <Link href={'/blogs/' + d.slug}
+                              className="font-sans text-xl text-slate-800 font-medium hover:text-slate-600">{d.title}</Link>
+                    </div>
+                    <p className="text-base text-slate-900">
+                        Lid est laborum et dolorum fuga. Et harum quidem rerum facilis est et
+                        expeditasi
+                        distincti...
+                    </p>
+                    <div className="flex flex-row justify-between">
+                        <p className="text-base text-slate-500 my-2">
+                            21 June 2020
+                        </p>
+                        <p className="text-base text-slate-500 my-2">
+                            Golang, Database
+                        </p>
+                        {/*<span className="text-slate-500 font-normal hover:text-slate-400"></span>*/}
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default CardBlog;
--- src/app/(main)/blogs/page.tsx
+++ src/app/(main)/blogs/page.tsx
@@ -1,12 +1,10 @@
 'use client'
 
-import Image from 'next/image'
-import Link from "next/link";
-import {useEffect, useState} from "react";
+import React, {useEffect, useState} from "react";
 import {GetPublicBlogs} from "@/api/blog";
-import Moment from "moment/moment";
+import CardBlog from "@/app/(main)/blogs/card";
 
-interface Blog {
+export interface Blog {
     title: string;
     slug: string;
     url_image: string;
@@ -23,20 +21,17 @@ const Blogs: React.FC = () => {
         {results: []}
     );
 
-
     useEffect(() => {
         const fetchDatasPublic = async () => {
             GetPublicBlogs().then(r => {
-                setTimeout(() => {
-                    r.data.forEach((d: Blog) => {
-                        d.url_image = process.env.NEXT_PUBLIC_API_HOST + d.url_image
+                r.data.forEach((d: Blog) => {
+                    d.url_image = process.env.NEXT_PUBLIC_API_HOST + d.url_image
 
-                        setBlogsData(prevState => ({
-                            results: [...prevState.results, d],
-                        }));
+                    setBlogsData(prevState => ({
+                        results: [...prevState.results, d],
+                    }));
 
-                    })
-                }, 350);
+                })
             })
         };
 
@@ -56,48 +51,9 @@ const Blogs: React.FC = () => {
                     <div className="flex flex-wrap grid-cols-3 gap-5">
                         {
                             blogsData.results.map((d, index) => (
-                                <div key={"item-" + index}
-                                     className="rounded-3xl w-full sm:w-1/2 md:w-1/3 self-stretch mb-2 hover:shadow-2xl border">
-                                    <div className="h-full">
-                                        <Link href={'/blogs/' + d.slug} className="">
-                                            <div className="relative w-full">
-                                                <Image className="w-full m-0 lazy rounded-2xl" src={d.url_image}
-                                                       width={100}
-                                                       height={100}
-                                                       quality={100} alt=""
-                                                       style={{
-                                                           objectFit: 'cover',
-                                                       }}
-                                                />
-                                            </div>
-
-                                        </Link>
-                                        <div className="px-6 py-5">
-                                            <div className="flex flex-col mb-2">
-                                                <Link href={'/blogs/' + d.slug}
-                                                      className="font-sans text-xl text-slate-800 font-medium hover:text-slate-600">{d.title}</Link>
-                                            </div>
-                                            <p className="text-base text-slate-900">
-                                                Lid est laborum et dolorum fuga. Et harum quidem rerum facilis est et
-                                                expeditasi
-                                                distincti...
-                                            </p>
-                                            <div className="flex flex-row justify-between">
-                                                <p className="text-base text-slate-500 my-2">
-                                                    21 June 2020
-                                                </p>
-                                                <p className="text-base text-slate-500 my-2">
-                                                    Golang, Database
-                                                </p>
-                                                {/*<span className="text-slate-500 font-normal hover:text-slate-400"></span>*/}
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
+                                <CardBlog key={index} d={d}/>
                             ))
                         }
-
-
                     </div>
                 </div>
             </div>
