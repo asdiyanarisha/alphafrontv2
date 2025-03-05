815489474ee46d6a483d3e0cdb7d2ecb3dccb2ad
risha.asdiyana <asdiyanarisha@gmail.com>
2025-03-05 12:40:15 +0700

    refactor: redesign blog list

--- src/app/(main)/blogs/card.tsx
+++ src/app/(main)/blogs/card.tsx
@@ -7,7 +7,7 @@ import Moment from 'moment';
 
 const CardBlog = ({ d }: {d: Blog}) => {
     return (
-        <div className="rounded-3xl xl:basis-1/4 lg:basis-1/3 self-stretch mx-1 mb-2 hover:shadow-2xl border shadow-lg border-1">
+        <div className="rounded-2xl xl:basis-1/4 lg:basis-1/3 self-stretch mx-1 mb-2 hover:shadow-2xl border border-solid shadow-lg border-slate-600">
             <div className="h-full">
                 <Link href={'/blogs/' + d.slug} className="">
                     <div className="relative w-full">
@@ -21,22 +21,32 @@ const CardBlog = ({ d }: {d: Blog}) => {
                         />
                     </div>
                 </Link>
-
                 <div className="px-5 py-4">
+                    <div className="font-mono text-base text-slate-700 italic">
+                        <span>
+                            {Moment(d.created_at).format("MMM DD,YYYY")}
+                        </span>
+                    </div>
                     <div className="flex flex-col mb-2">
                         <Link href={'/blogs/' + d.slug}
-                              className="font-sans text-xl text-slate-800 font-medium hover:text-slate-600 capitalize">{d.title}</Link>
+                              className="font-sans text-xl text-slate-800 font-semibold hover:text-slate-600 capitalize">{d.title}</Link>
                     </div>
-                    <div className="my-1 h-20 md:h-30 text-base text-slate-900 text-wrap">
+                    <div className="h-20 md:h-30 text-base text-slate-900 text-wrap">
                         <p>{d.description}</p>
                     </div>
-                    <div className="flex flex-row justify-between">
-                        <p className="text-base text-slate-500 my-2">
-                            {Moment(d.created_at).format("DD MMM YYYY")}
-                        </p>
-                        <p className="text-base text-slate-500 my-2">
-                            {d.tags.join(",")}
-                        </p>
+                    <div className="flex flex-row justify-end">
+                        <div className="flex gap-1 text-base text-white">
+                            {
+                                d.tags.map((val, indexTag) => (
+                                    <div key={"tag-"+indexTag}
+                                    className="bg-black rounded-3xl capitalize border border-solid border-slate-950">
+                                        <span className="m-3">
+                                            #{val}
+                                        </span>
+                                    </div>
+                                ))
+                            }
+                        </div>
                     </div>
                 </div>
             </div>
