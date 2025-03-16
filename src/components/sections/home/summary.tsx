1f3cff3ea845f77120266ecdbc89bd52dc731077
risha.asdiyana <asdiyanarisha@gmail.com>
2025-03-16 20:33:38 +0700

    feat: add experience in summary section

--- src/components/sections/home/summary.tsx
+++ src/components/sections/home/summary.tsx
@@ -24,7 +24,7 @@ const Summary = () => {
     return (
         <div ref={domRef} className={`mt-52 pt-36 shadow-2xl fade-in-section ${isVisible ? 'is-visible' : ''}`}>
             <div className="flex items-center justify-center font-sans bg-black text-white">
-                <div className="xl:w-1/2 my-20 mt-10 w-full">
+                <div className="xl:w-1/2 my-20 mt-20 w-full">
                     <div className="flex flex-row justify-center">
                         <Avatar className="w-[10rem] h-[10rem]">
                             <AvatarImage src="https://avatars.githubusercontent.com/u/19662749?v=4"/>
@@ -36,7 +36,7 @@ const Summary = () => {
                             <h2 className="text-2xl font-semibold text-slate-300">About Me</h2>
                         </div>
                         <div
-                            className="xl:ml-28 mx-10 text-lg xl:w-3/4 w-10/12 md:text-justify flex justify-center flex-col text-slate-300">
+                            className="content-summary">
                             <p className="text-wrap">
                                 Hello my name Risha Asdiyana Rifi, and you can call me Risha :).
                                 I am a software engineer with 8+ years eperiences in full-stack development.
@@ -56,12 +56,41 @@ const Summary = () => {
                                 and optimizing performance load process api.
                             </p>
                             <p className="text-wrap mt-8">
-                                Outside of work, I enjoy staying up to date with emerging technologies and have built side projects exploring frontend technologies such as react and tailwind.
+                                Outside of work, I enjoy staying up to date with emerging technologies and have built
+                                side projects exploring frontend technologies such as react and tailwind.
                             </p>
                         </div>
                     </div>
                     <div className="py-14">
-                        <div className="w-full flex justify-center mb-5">
+                        <div className="w-full flex justify-center my-10">
+                            <h2 className="text-2xl font-semibold text-slate-300">My Experience</h2>
+                        </div>
+                        <div
+                            className="content-summary">
+                            <Link href="https://komerce.id" target="_blank">
+                                <div className="flex flex-col lg:flex-row mb-5" id="experience">
+                                    <div className="lg:w-1/4 text-base">
+                                        <span>2023 — Present</span>
+                                    </div>
+                                    <div className="lg:w-3/4 flex flex-col">
+                                        <h2 className="text-base font-semibold">
+                                            <div className="hover:text-black hover:bg-white">
+                                                Senior Backend Engineer - Komerce
+                                            </div>
+                                        </h2>
+                                        <p className="text-wrap mt-1 text-base">
+                                            Outside of work, I enjoy staying up to date with emerging technologies and
+                                            have
+                                            built
+                                            side projects exploring frontend technologies such as react and tailwind.
+                                        </p>
+                                    </div>
+                                </div>
+                            </Link>
+                        </div>
+                    </div>
+                    <div className="py-14">
+                        <div className="w-full flex justify-center my-10">
                             <h2 className="text-2xl font-semibold text-slate-300">Let's be Connect!</h2>
                         </div>
                         <div className="flex pb-3 justify-center" id="icon">
