4320dccba1cd1e644000b7b455240148247a010e
risha.asdiyana <asdiyanarisha@gmail.com>
2025-05-31 21:06:19 +0700

    fix layout projects

--- src/app/(main)/projects/page.tsx
+++ src/app/(main)/projects/page.tsx
@@ -12,7 +12,7 @@ const Projects: React.FC = () => {
                 <div className="my-10 font-sans xl:ml-2 lg:ml-36 ml-20 justify-center">
                     <h1 className="text-4xl font-bold text-slate-600 font-sans">My Projects</h1>
                 </div>
-                <div className="gap-2 mb-14 xl:w-[1380]">
+                <div className="gap-2 mb-14 xl:w-[1380px]">
                     <section className="blog text-gray-700 body-font">
                         <div className="container px-5 py-12 mx-auto">
                             <div className="flex flex-wrap sm:-m-4 -mx-4 -mb-10 -mt-4">
